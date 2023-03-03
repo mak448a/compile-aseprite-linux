412340f53fad267c5bf042f990e7a5ebab275486
mak448a <94062293+mak448a@users.noreply.github.com>
2023-03-03 11:20:26 -0600

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
 
+wget https://github.com/aseprite/skia/releases/download/m102-861e4743af/Skia-Linux-Release-x64-libc++.zip
+
+mkdir -p ~/deps/skia
+unzip Skia-Linux-Release-x64-libc++.zip -d ~/deps/skia
+
+rm Skia-Linux-Release-x64-libc++.zip
