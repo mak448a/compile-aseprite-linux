5ec5eb32c5081a49c400681f8e10a929d9dfb341
mak448a <94062293+mak448a@users.noreply.github.com>
2023-03-03 11:23:27 -0600

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+# Download skia
 wget https://github.com/aseprite/skia/releases/download/m102-861e4743af/Skia-Linux-Release-x64-libc++.zip
-
 mkdir -p ~/deps/skia
 unzip Skia-Linux-Release-x64-libc++.zip -d ~/deps/skia
-
+# Clean up zip file
 rm Skia-Linux-Release-x64-libc++.zip
+
+# Clone aseprite
+git clone --recursive https://github.com/aseprite/aseprite.git
+
+# Install deps
+sudo dnf install -y gcc-c++ clang libcxx-devel cmake ninja-build libX11-devel libXcursor-devel libXi-devel mesa-libGL-devel fontconfig-devel
