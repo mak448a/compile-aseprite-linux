5000249f385f796f73e21ab70b1d724e47c49a9d
Marcus Minhorst <marcus.minhorst@gmail.com>
2025-04-06 20:13:01 -0400

    Added some error detection

--- compile.sh
+++ compile.sh
@@ -57,36 +57,47 @@ fi
 
 # Install dependencies
 if [[ $package_man == "dnf" ]]; then
-  sudo dnf install -y gcc-c++ clang libcxx-devel cmake ninja-build libX11-devel libXcursor-devel libXi-devel mesa-libGL-devel fontconfig-devel git
+    sudo dnf install -y gcc-c++ clang libcxx-devel cmake ninja-build libX11-devel libXcursor-devel libXi-devel mesa-libGL-devel fontconfig-devel git
 elif [[ $package_man == "apt" ]]; then
-  sudo apt-get install -y g++ clang libc++-dev libc++abi-dev cmake ninja-build libx11-dev libxcursor-dev libxi-dev libgl1-mesa-dev libfontconfig1-dev git
+    sudo apt-get install -y g++ clang libc++-dev libc++abi-dev cmake ninja-build libx11-dev libxcursor-dev libxi-dev libgl1-mesa-dev libfontconfig1-dev git
 fi
 
+[[ $? == 0 ]] \
+    || { echo "failed to install dependencies" >&2 ; exit 1 ; }
+
 # Clone aseprite
-git clone --recursive https://github.com/aseprite/aseprite.git --depth=1
+git clone --recursive https://github.com/aseprite/aseprite.git --depth=1 \
+    || { echo "unable to clone code base" >&2 ; exit 1 ; }
 
 # Download skia
-wget https://github.com/aseprite/skia/releases/download/m102-861e4743af/Skia-Linux-Release-x64-libc++.zip
-mkdir ./skia
-unzip Skia-Linux-Release-x64-libc++.zip -d ./skia
+wget https://github.com/aseprite/skia/releases/download/m102-861e4743af/Skia-Linux-Release-x64-libc++.zip \
+    || { echo "failed to download skia" >&2 ; exit 1 ; }
+mkdir ./skia && unzip Skia-Linux-Release-x64-libc++.zip -d ./skia \
+    || { echo "failed to extract skia" >&2 ; exit 1 ; }
 
 echo "Finished downloading! Time to compile."
 
-mkdir aseprite/build
+mkdir aseprite/build \
+    || { echo "failed create build folder" >&2 ; exit 1 ; }
 pushd aseprite/build
+
 export CC=clang
 export CXX=clang++
 cmake \
-  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-  -DCMAKE_CXX_FLAGS:STRING=-stdlib=libc++ \
-  -DCMAKE_EXE_LINKER_FLAGS:STRING=-stdlib=libc++ \
-  -DLAF_BACKEND=skia \
-  -DSKIA_DIR="${WORK_DIR}/skia" \
-  -DSKIA_LIBRARY_DIR="${WORK_DIR}/skia/out/Release-x64" \
-  -DSKIA_LIBRARY="${WORK_DIR}/skia/out/Release-x64/libskia.a" \
-  -G Ninja \
-  ..
-ninja aseprite
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    -DCMAKE_CXX_FLAGS:STRING=-stdlib=libc++ \
+    -DCMAKE_EXE_LINKER_FLAGS:STRING=-stdlib=libc++ \
+    -DLAF_BACKEND=skia \
+    -DSKIA_DIR="${WORK_DIR}/skia" \
+    -DSKIA_LIBRARY_DIR="${WORK_DIR}/skia/out/Release-x64" \
+    -DSKIA_LIBRARY="${WORK_DIR}/skia/out/Release-x64/libskia.a" \
+    -G Ninja \
+    .. \
+    || { echo "configuration failed" >&2 ; exit 1 ; }
+
+ninja aseprite \
+    || { echo "compilation failed" >&2 ; exit 1 ; }
+
 popd
 
 rm -rf "${INSTALL_DIR}" \
@@ -94,12 +105,12 @@ rm -rf "${INSTALL_DIR}" \
 mkdir -p "${INSTALL_DIR}" "${BINARY_DIR}" "${LAUNCHER_DIR}" "${ICON_DIR}" \
     || { echo "unable to create install folder" >&2 ; exit 1 ; }
 
-mv aseprite/build/bin/* "${INSTALL_DIR}"
-
-touch "${SIGNATURE_FILE}"
-ln -s "${INSTALL_DIR}/aseprite" "${BINARY_FILE}"
-ln -s "${INSTALL_DIR}/data/icons/ase256.png" "${ICON_FILE}"
-cp "${WORK_DIR}/aseprite/src/desktop/linux/aseprite.desktop" "${LAUNCHER_FILE}"
+{ mv aseprite/build/bin/* "${INSTALL_DIR}" \
+    && touch "${SIGNATURE_FILE}" \
+    && ln -sf "${INSTALL_DIR}/aseprite" "${BINARY_FILE}" \
+    && ln -sf "${INSTALL_DIR}/data/icons/ase256.png" "${ICON_FILE}" \
+    && cp -f "${WORK_DIR}/aseprite/src/desktop/linux/aseprite.desktop" "${LAUNCHER_FILE}" \
+; } || { echo "failed to complete install" >&2 ; exit 1 ; }
 
 echo "Done compiling!"
 echo "The executable is stored in '${INSTALL_DIR}'. Have fun!"
