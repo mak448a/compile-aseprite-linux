6f8054e13338099340de4863a0e0df4688d22246
PepeV_nRT <153303346+pepev-nrt@users.noreply.github.com>
2025-10-30 04:35:11 +0100

    Rework of the compile script (#17)
    * Simplified the command to launch the compiler
    
    * Corrected error in the path of the executable in the README
    
    * Download Aseprite source code from the latest stable .zip package instead of cloning the developing branch
    
    * Rearanged so the source code is downloaded first and then the dependencies are installed
    
    * Reads the dependencies from INSTALL.md instead of having them hard-coded in the script.
    
    * Compile Aseprite with the provided build.sh script in the Source code
    
    * Complete the .desktop file with the correct values
    
    * Corrected typos
    
    * Icon is no longer copied to the ~/.local/share/icons as is already in the aseprite data folder and is only used in the .desktop file
    
    * Set the option --auto for the .build.sh script
    
    * Arch and Manjaro support added
    
    * Pacman confirmation for deps skipped
    
    * Arch support fixed
    
    * Option --norun in build.sh to not launch aseprite before finishing the script
    
    * Modified the comments in compile.sh regarding the pacman support

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # Compile Aseprite on Linux
 Here's how to compile aseprite for free on Fedora, Ubuntu, and more! If you want to support the developers, buy it from [Aseprite.org](https://aseprite.org)!
 
-To run the script, [download the script here](compile.sh) by clicking view raw and then CTRL+S to save the file.
-Go to your downloads folder, open a terminal window, and paste this.
-```shell
-chmod +x compile.sh && ./compile.sh
+To run the script, simply open a terminal window, and paste this:
+```bash
+bash -c "$(curl -sSf 'https://raw.githubusercontent.com/mak448a/compile-aseprite-linux/refs/heads/main/compile.sh')"
 ```
 
 Please consider giving this repo a star if you found it helpful.
@@ -12,7 +11,7 @@ Please consider giving this repo a star if you found it helpful.
 If you encounter any errors, please report them in the issues tab.
 
 ## Where is the executable?
-The executable is stored in ~/aseprite. Have fun!
+The executable is stored in ~/.local/share/aseprite. Have fun!
 
 ## How do I run Aseprite?
 This script automatically adds Aseprite to your desktop environment's application launcher and to $PATH.
--- compile.sh
+++ compile.sh
@@ -5,21 +5,20 @@
 INSTALL_DIR="${XDG_DATA_HOME}/aseprite"
 BINARY_DIR="${HOME}/.local/bin"
 LAUNCHER_DIR="${XDG_DATA_HOME}/applications"
-ICON_DIR="${XDG_DATA_HOME}/icons"
 
 SIGNATURE_FILE="${INSTALL_DIR}/compile-aseprite-linux"
 BINARY_FILE="${BINARY_DIR}/aseprite"
 LAUNCHER_FILE="${LAUNCHER_DIR}/aseprite.desktop"
-ICON_FILE="${ICON_DIR}/aseprite.png"
+ICON_FILE="${INSTALL_DIR}/data/icons/ase256.png"
 
 if [[ -f "${SIGNATURE_FILE}" ]] ; then
-    read -e -p "Aseprite already installed. Update? (y/n): " choice
+    read -e -p "Aseprite already installed. Update? (y/N): " choice
     [[ "${choice}" == [Yy]* ]] \
         || exit 0
 else
     [[ -d "${INSTALL_DIR}" ]] \
         && { echo "Aseprite already installed to '${INSTALL_DIR}'. Aborting" >&2 ; exit 1 ; }
-    { [[ -f "${LAUNCHER_FILE}" ]] || [[ -f "${BINARY_FILE}" ]] || [[ -f "${ICON_FILE}" ]] ; } \
+    { [[ -f "${LAUNCHER_FILE}" ]] || [[ -f "${BINARY_FILE}" ]] ; } \
         && { echo "Other aseprite data already installed to home directory. Aborting" >&2 ; exit 1 ; }
 fi
 
@@ -47,80 +46,78 @@ trap "cleanup" EXIT
 
 pushd "${WORK_DIR}"
 
+# Download latest version of aseprite
+# SOURCE_CODE is a link like https://github.com/aseprite/aseprite/releases/download/vX.X.X.X/Aseprite-vX.X.X.X-Source.zip
+SOURCE_CODE=$(curl -s "https://api.github.com/repos/aseprite/aseprite/releases/latest" | awk '/browser_download_url/ {print $2}' | tr -d \")
+
+wget -q $SOURCE_CODE \
+    || { echo "Unable to download the latest version of Aseprite." >&2 ; exit 1 ; }
+echo "Aseprite downloaded from: ${SOURCE_CODE}"
+
+# FILE is a filename like Aseprite-vX.X.X.X-Source.zip
+FILE=$(echo $SOURCE_CODE | awk -F/ '{print $NF}')
+
+# Unzip the source code
+unzip -q $FILE -d aseprite \
+    || { echo "Unable to decompress the source code, make sure you have the unzip package installed." >&2 ; exit 1 ; }
+echo "${FILE} decompresed."
+
 # Check distro
 os_name=$(grep 'NAME=' /etc/os-release | head -n 1 | sed 's/NAME=//' | tr -d '"')
-
-echo "Enter sudo password to install dependencies. This is also a good time to plug in your computer, since compiling will take a long time."
 
 # Assign package manager to a variable
 if [[ "$os_name" == *"Fedora"* ]]; then
     package_man="dnf"
 elif [[ $os_name == *"Debian"* ]] || [[ $os_name == *"Ubuntu"* ]] || [[ $os_name == *"Mint"* ]]; then
     package_man="apt"
+elif [[ $os_name == *"Arch"* ]] || [[ $os_name == *"Manjaro"* ]]; then
+    package_man="pacman"
 else
-    echo "Unsupported distro! If your distro supports APT or DNF, please manually modify the script to set os_name='Ubuntu' for apt, or os_name='Fedora'. You can also open an issue ticket."
+    echo "Unsupported distro! If your distro supports APT, DNF or PACMAN, please manually modify the script to set os_name='Ubuntu' for apt, os_name='Fedora' for dnf or os_name='Arch' for pacman. You can also open an issue ticket."
     echo "Stopped installation!"
     exit 1
 fi
 
+echo "Enter sudo password to install dependencies. This is also a good time to plug in your computer, since compiling will take a long time."
+
 # Install dependencies
 if [[ $package_man == "dnf" ]]; then
-    sudo dnf install -y git \
-        gcc-c++ clang libcxx-devel cmake ninja-build libX11-devel libXcursor-devel libXi-devel libXrandr-devel mesa-libGL-devel fontconfig-devel
+    cat aseprite/INSTALL.md | grep -m1 "sudo dnf install" | bash 
 elif [[ $package_man == "apt" ]]; then
-    sudo apt-get install -y git\
-        g++ clang cmake ninja-build libx11-dev libxcursor-dev libxi-dev libxrandr-dev libgl1-mesa-dev libfontconfig1-dev
+    cat aseprite/INSTALL.md | grep -m1 "sudo apt-get install" | bash
+elif [[ $package_man == "pacman" ]]; then
+    deps=$(cat aseprite/INSTALL.md | grep -m1 "sudo pacman -S")
+    deps=${deps/-S/-S --needed --noconfirm} 
+    bash -c "$deps"
+
 fi
 
 [[ $? == 0 ]] \
     || { echo "Failed to install dependencies." >&2 ; exit 1 ; }
 
-# Clone aseprite
-git clone --recursive https://github.com/aseprite/aseprite.git --depth=1 \
-    || { echo "Unable to clone Aseprite repo." >&2 ; exit 1 ; }
+pushd aseprite
 
-# Download skia
-wget -O skia.zip https://github.com/aseprite/skia/releases/download/m124-08a5439a6b/Skia-Linux-Release-x64.zip \
-    || { echo "Failed to download skia." >&2 ; exit 1 ; }
-mkdir ./skia && unzip skia.zip -d ./skia \
-    || { echo "Failed to extract skia." >&2 ; exit 1 ; }
-
-echo "Finished downloading! Time to compile."
-
-mkdir aseprite/build \
-    || { echo "Failed create build folder." >&2 ; exit 1 ; }
-pushd aseprite/build
-
-export CC=clang
-export CXX=clang++
-cmake \
-    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-    -DCMAKE_CXX_FLAGS:STRING=-stdlib=libstdc++ \
-    -DCMAKE_EXE_LINKER_FLAGS:STRING=-stdlib=libstdc++ \
-    -DLAF_BACKEND=skia \
-    -DSKIA_DIR="${WORK_DIR}/skia" \
-    -DSKIA_LIBRARY_DIR="${WORK_DIR}/skia/out/Release-x64" \
-    -DSKIA_LIBRARY="${WORK_DIR}/skia/out/Release-x64/libskia.a" \
-    -G Ninja \
-    .. \
-    || { echo "Configuration failed." >&2 ; exit 1 ; }
-
-ninja aseprite \
+# Compile Aseprite with the provided build.sh script in the source code
+./build.sh --auto --norun \
     || { echo "Compilation failed." >&2 ; exit 1 ; }
 
 popd
 
 rm -rf "${INSTALL_DIR}" \
     || { echo "Unable to clean up old install." >&2 ; exit 1 ; }
-mkdir -p "${INSTALL_DIR}" "${BINARY_DIR}" "${LAUNCHER_DIR}" "${ICON_DIR}" \
+mkdir -p "${INSTALL_DIR}" "${BINARY_DIR}" "${LAUNCHER_DIR}" \
     || { echo "Unable to create install folder." >&2 ; exit 1 ; }
 
 { mv aseprite/build/bin/* "${INSTALL_DIR}" \
     && touch "${SIGNATURE_FILE}" \
     && ln -sf "${INSTALL_DIR}/aseprite" "${BINARY_FILE}" \
-    && ln -sf "${INSTALL_DIR}/data/icons/ase256.png" "${ICON_FILE}" \
     && cp -f "${WORK_DIR}/aseprite/src/desktop/linux/aseprite.desktop" "${LAUNCHER_FILE}" \
 ; } || { echo "Failed to complete install." >&2 ; exit 1 ; }
+
+# Replace the values on the .desktop file to the correct ones
+sed -i "s|$(grep -m1 TryExec= "${LAUNCHER_FILE}")|TryExec=$BINARY_FILE|g" "${LAUNCHER_FILE}"
+sed -i "s|$(grep -m1 Exec= "${LAUNCHER_FILE}")|Exec=$BINARY_FILE %U|g" "${LAUNCHER_FILE}"
+sed -i "s|$(grep -m1 Icon= "${LAUNCHER_FILE}")|Icon=$ICON_FILE|g" "${LAUNCHER_FILE}"
 
 echo "Done compiling!"
 echo "The executable is stored in '${INSTALL_DIR}'. Have fun!"
